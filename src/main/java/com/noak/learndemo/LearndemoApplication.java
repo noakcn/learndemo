a8dfb3ff4e4357db000a023e03db3d237937a925
yewangwang <yewangwang@souche.com>
2018-07-03 16:57:06 +0800

    init project

---
+++ src/main/java/com/noak/learndemo/LearndemoApplication.java
@@ -0,0 +1,12 @@
+package com.noak.learndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearndemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearndemoApplication.class, args);
+    }
+}
